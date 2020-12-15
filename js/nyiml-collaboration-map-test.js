a85f750eb2ad7371c5a6028b69751001a379a8dd
Kyle Cranmer <kyle.cranmer@nyu.edu>
2020-12-15 16:16:42 -0500

    Update documentation

--- js/nyiml-collaboration-map-test.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  plotCollaborationMap();
-});
-function plotCollaborationMap()
-{
-  d3.json("/downloads/files/nyiml.json", function(dataJson) {
-      var plot = new CollaborationMap("graph", "graph-info", dataJson);
-  });
-}
